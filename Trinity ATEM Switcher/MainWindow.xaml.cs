6a908892f41f2c9fe74bedacf0df9f2d843bf6d1
Brett Halsey <bhalsey@gmail.com>
2018-04-04 11:18:38 -0500

    1.  connected to ATEM - manually right now 2.  get source names on buttons 3.  add program and preview button commands

--- Trinity ATEM Switcher/MainWindow.xaml.cs
+++ Trinity ATEM Switcher/MainWindow.xaml.cs
@@ -12,17 +12,263 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BMDSwitcherAPI;
+using System.Runtime.InteropServices;
 
 namespace Trinity_ATEM_Switcher
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IBMDSwitcherDiscovery m_switcherDiscovery;
+        private IBMDSwitcher m_switcher;
+        private IBMDSwitcherMixEffectBlock m_mixEffectBlock1;
+
+        private SwitcherMonitor m_switcherMonitor;
+        private MixEffectBlockMonitor m_mixEffectBlockMonitor;
+        private IBMDSwitcherTransitionParameters m_transition;
+        private bool m_moveSliderDownwards = false;
+        private bool m_currentTransitionReachedHalfway = false;
+        private _BMDSwitcherTransitionStyle existing_style;
+        private List<InputMonitor> m_inputMonitors = new List<InputMonitor>();
         public MainWindow()
         {
             InitializeComponent();
+
+            m_switcherMonitor = new SwitcherMonitor();
+            
+
+            m_mixEffectBlockMonitor = new MixEffectBlockMonitor();
+            
+
+            m_switcherDiscovery = new CBMDSwitcherDiscovery();
+            if (m_switcherDiscovery == null)
+            {
+                MessageBox.Show("Could not create Switcher Discovery Instance.\nATEM Switcher Software may not be installed.", "Error");
+                Environment.Exit(1);
+            }
+
+            SwitcherDisconnected();		// start with switcher disconnected
+
+            _BMDSwitcherConnectToFailure failReason = 0;
+            string address = "192.168.111.102";
+
+            try
+            {
+                // Note that ConnectTo() can take several seconds to return, both for success or failure,
+                // depending upon hostname resolution and network response times, so it may be best to
+                // do this in a separate thread to prevent the main GUI thread blocking.
+                m_switcherDiscovery.ConnectTo(address, out m_switcher, out failReason);
+            }
+            catch (COMException)
+            {
+                // An exception will be thrown if ConnectTo fails. For more information, see failReason.
+                switch (failReason)
+                {
+                    case _BMDSwitcherConnectToFailure.bmdSwitcherConnectToFailureNoResponse:
+                        MessageBox.Show("No response from Switcher", "Error");
+                        break;
+                    case _BMDSwitcherConnectToFailure.bmdSwitcherConnectToFailureIncompatibleFirmware:
+                        MessageBox.Show("Switcher has incompatible firmware", "Error");
+                        break;
+                    default:
+                        MessageBox.Show("Connection failed for unknown reason", "Error");
+                        break;
+                }
+                return;
+            }
+
+            SwitcherConnected();
+        }
+
+
+        private void OnInputLongNameChanged(object sender, object args)
+        {
+            //this.Invoke((Action)(() => UpdatePopupItems()));
+        }
+
+        private void SwitcherConnected()
+        {
+            //buttonConnect.Enabled = false;
+
+            // Get the switcher name:
+            string switcherName;
+            m_switcher.GetProductName(out switcherName);
+            //textBoxSwitcherName.Text = switcherName;
+            
+            // Install SwitcherMonitor callbacks:
+            m_switcher.AddCallback(m_switcherMonitor);
+
+            // We create input monitors for each input. To do this we iterate over all inputs:
+            // This will allow us to update the combo boxes when input names change:
+            IBMDSwitcherInputIterator inputIterator = null;
+            IntPtr inputIteratorPtr;
+            Guid inputIteratorIID = typeof(IBMDSwitcherInputIterator).GUID;
+            m_switcher.CreateIterator(ref inputIteratorIID, out inputIteratorPtr);
+            if (inputIteratorPtr != null)
+            {
+                inputIterator = (IBMDSwitcherInputIterator)Marshal.GetObjectForIUnknown(inputIteratorPtr);
+            }
+
+            if (inputIterator != null)
+            {
+                IBMDSwitcherInput input;
+                inputIterator.Next(out input);
+                while (input != null)
+                {
+                    InputMonitor newInputMonitor = new InputMonitor(input);
+                    input.AddCallback(newInputMonitor);
+                    newInputMonitor.LongNameChanged += new SwitcherEventHandler(OnInputLongNameChanged);
+
+                    m_inputMonitors.Add(newInputMonitor);
+
+                    inputIterator.Next(out input);
+                }
+            }
+
+            // We want to get the first Mix Effect block (ME 1). We create a ME iterator,
+            // and then get the first one:
+            m_mixEffectBlock1 = null;
+
+            IBMDSwitcherMixEffectBlockIterator meIterator = null;
+            IntPtr meIteratorPtr;
+            Guid meIteratorIID = typeof(IBMDSwitcherMixEffectBlockIterator).GUID;
+            m_switcher.CreateIterator(ref meIteratorIID, out meIteratorPtr);
+            if (meIteratorPtr != null)
+            {
+                meIterator = (IBMDSwitcherMixEffectBlockIterator)Marshal.GetObjectForIUnknown(meIteratorPtr);
+            }
+
+            if (meIterator == null)
+                return;
+
+            if (meIterator != null)
+            {
+                meIterator.Next(out m_mixEffectBlock1);
+            }
+
+            if (m_mixEffectBlock1 == null)
+            {
+                MessageBox.Show("Unexpected: Could not get first mix effect block", "Error");
+                return;
+            }
+
+            // Install MixEffectBlockMonitor callbacks:
+            m_mixEffectBlock1.AddCallback(m_mixEffectBlockMonitor);
+
+            //MixEffectBlockSetEnable(true);
+            getInputNames();
+            //UpdateTransitionFramesRemaining();
+            //UpdateSliderPosition();
+        }
+
+        private void getInputNames()
+        {
+
+            // Get an input iterator.
+            IBMDSwitcherInputIterator inputIterator = null;
+            IntPtr inputIteratorPtr;
+            Guid inputIteratorIID = typeof(IBMDSwitcherInputIterator).GUID;
+            m_switcher.CreateIterator(ref inputIteratorIID, out inputIteratorPtr);
+            if (inputIteratorPtr != null)
+            {
+                inputIterator = (IBMDSwitcherInputIterator)Marshal.GetObjectForIUnknown(inputIteratorPtr);
+            }
+
+            if (inputIterator == null)
+                return;
+
+            IBMDSwitcherInput input;
+            inputIterator.Next(out input);
+            while (input != null)
+            {
+                string inputName;
+                long inputId;
+
+                input.GetInputId(out inputId);
+                input.GetLongName(out inputName);
+                Console.WriteLine(inputId + " " + inputName);
+                switch (inputId)
+                {
+                    case 2:
+                        progBut1.Content = inputName;
+                        prevBut1.Content = inputName;
+                        break;
+                    case 3:
+                        progBut2.Content = inputName;
+                        prevBut2.Content = inputName;
+                        break;
+                    case 4:
+                        progBut3.Content = inputName;
+                        prevBut3.Content = inputName;
+                        break;
+                    case 5:
+                        progBut4.Content = inputName;
+                        prevBut4.Content = inputName;
+                        break;
+                    case 6:
+                        progBut5.Content = inputName;
+                        prevBut5.Content = inputName;
+                        break;
+                    case 3010:
+                        progBut6.Content = inputName;
+                        prevBut6.Content = inputName;
+                        break;
+                }
+               
+                inputIterator.Next(out input);
+            }
+        }
+
+        private void SwitcherDisconnected()
+        {
+
+            if (m_mixEffectBlock1 != null)
+            {
+                // Remove callback
+                m_mixEffectBlock1.RemoveCallback(m_mixEffectBlockMonitor);
+
+                // Release reference
+                m_mixEffectBlock1 = null;
+            }
+
+            if (m_switcher != null)
+            {
+                // Remove callback:
+                m_switcher.RemoveCallback(m_switcherMonitor);
+
+                // release reference:
+                m_switcher = null;
+            }
+        }
+
+
+
+        private void progBut_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var butVal = button.Tag;
+            int butNum = Convert.ToInt32(butVal);
+            if (m_mixEffectBlock1 != null)
+            {
+                m_mixEffectBlock1.SetInt(_BMDSwitcherMixEffectBlockPropertyId.bmdSwitcherMixEffectBlockPropertyIdProgramInput,
+                   butNum);
+            }
+        }
+
+        private void prevBut_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var butVal = button.Tag;
+            int butNum = Convert.ToInt32(butVal);
+            if (m_mixEffectBlock1 != null)
+            {
+                m_mixEffectBlock1.SetInt(_BMDSwitcherMixEffectBlockPropertyId.bmdSwitcherMixEffectBlockPropertyIdPreviewInput,
+                   butNum);
+            }
         }
     }
 }
